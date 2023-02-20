8763ddab4095bb5fbf801842cee85d901d5ba2da
Jason Zhao <jicheng@ualberta.ca>
2023-02-19 17:27:09 -0700

    end of 10, start a new project

---
+++ 06-usereducer-starting-project/src/components/UI/Input/Input.js
@@ -0,0 +1,37 @@
+import React,{useRef, useImperativeHandle}from "react";
+import classes from "./Input.module.css";
+
+
+const Input = React.forwardRef((props, ref) => {
+    const inputref =useRef();
+
+    const active = () =>{
+        inputref.current.focus();
+    };
+    useImperativeHandle(ref, ()=>{
+        return{
+            focus:active
+        };
+    });
+
+
+  return (
+    <div
+      className={`${classes.control} ${
+        props.isValid === false ? classes.invalid : ""
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref ={inputref}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
